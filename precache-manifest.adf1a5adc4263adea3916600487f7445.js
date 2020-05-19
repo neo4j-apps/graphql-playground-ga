b880e4ea4383d98785d252d21f9059fb68ddd757
ABK <akollegger@gmail.com>
2020-05-19 20:47:18 +0200

    Updates

---
+++ precache-manifest.adf1a5adc4263adea3916600487f7445.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48777c113af1154b603a2f47a43abba8",
+    "url": "/graphql-playground-ga/index.html"
+  },
+  {
+    "revision": "768e9739cfb1b718821f",
+    "url": "/graphql-playground-ga/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "12f60097a64fafee5552",
+    "url": "/graphql-playground-ga/static/js/2.a2d35e17.chunk.js"
+  },
+  {
+    "revision": "475031df1e69bf6380e1443e5ec73a7a",
+    "url": "/graphql-playground-ga/static/js/2.a2d35e17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "768e9739cfb1b718821f",
+    "url": "/graphql-playground-ga/static/js/main.7ed46f4a.chunk.js"
+  },
+  {
+    "revision": "9107be854c04c59df673",
+    "url": "/graphql-playground-ga/static/js/runtime-main.17aa797a.js"
+  },
+  {
+    "revision": "57ee3b6084cb9d3c754cc12d25a98035",
+    "url": "/graphql-playground-ga/static/media/logo.57ee3b60.png"
+  }
+]);
